a6b38ddb9ccf3c316b3449cddcb6a65101bce139
EdsonGomesJr <edyz86@hotmail.com>
2023-05-29 09:16:24 -0300

    NavBar completed

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -1,3 +1,15 @@
+'use client'
+
+import Image from 'next/image'
+
 export function Avatar() {
-  return <div>Avatar</div>
+  return (
+    <Image
+      src="/images/placeholder.jpg"
+      className="rounded-full"
+      width={30}
+      height={30}
+      alt="Avatar"
+    />
+  )
 }
---
+++ app/components/navbar/MenuItem.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+interface MenuItemProps {
+  onClick: () => void
+  label: string
+}
+export function MenuItem({ onClick, label }: MenuItemProps) {
+  return (
+    <div
+      onClick={onClick}
+      className="px-4 py-3 font-semibold transition hover:bg-neutral-100"
+    >
+      {label}
+    </div>
+  )
+}
